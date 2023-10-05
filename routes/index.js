9f649c745b9953ea210aff30ea3a56336aa7d670
Abel Antonio Ulysses Aguilar Aguirre <104412233+AbelAguilarAguirre@users.noreply.github.com>
2023-10-05 13:46:19 -1000

    Routes index (#2)
    * server.js complete
    
    * server fix
    
    * connect to DB
    
    * finished with routes

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.use('/', require('./swagger'));
+router.use('/employees', require('./employees'));
+router.use('/menu', require('./menu'));
+router.use('/reservations', require('./reservations'));
+router.use('/inventory', require('./inventory'));
+
+module.exports = router;
