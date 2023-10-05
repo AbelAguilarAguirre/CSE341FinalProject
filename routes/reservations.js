68aa9ba3fc7795e4d942a794dc46db6d3edf78d7
Abel Antonio Ulysses Aguilar Aguirre <104412233+AbelAguilarAguirre@users.noreply.github.com>
2023-10-05 13:47:53 -1000

    Routes employees reservations (#3)
    * finished with routes
    
    * completed routes for reservations and employees

---
+++ routes/reservations.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const reservationsController = require('../controllers/reservations');
+const { isAuthenticated } = require('../middleware/authenticate');
+const validation = require('../middleware/validate');
+
+router.get('/', reservationsController.getAll);
+
+router.get('/:id', reservationsController.getSingle);
+
+router.post('/', isAuthenticated, validation.saveReservation, reservationsController.createReservation);
+
+router.put('/:id', isAuthenticated, validation.saveReservation, reservationsController.updateReservation);
+
+router.delete('/:id', isAuthenticated, reservationsController.deleteReservation);
+
+module.exports = router;
